4b45bd302ac544c7c029ce1c15484f75fbbc8860
haazimiapp <centroculturalarabeyguyanes@gmail.com>
2026-03-23 23:30:32 -0400

    Update config.js

--- src/data/config.js
+++ src/data/config.js
@@ -1,4 +1,4 @@
-export const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbw3kLcZu7y1AoUtThJbkpUiTSdZM1qke4Yuq-7IsXKCJ91LtjW3mshFGQj0z62WUO8l/exec";
+export const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbws7pW27E_djBBT1zcG0j4x9A0c0bsTjo5Gc422ZhSu0SOz1n3nbHSHzvtTs77eALUT/exec";
 
 export const COUNTRY_CENTRES = {
   "South Africa": ["Ext. 1 (Lenasia)", "Ext. 13 (Lenasia)"],
